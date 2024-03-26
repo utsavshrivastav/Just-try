8762c25f1dd7110cbfebc17814f9a14a6f688065
utsavshrivastav <thevickyshrivastav@gmail.com>
2024-03-26 05:50:40 +0530

    java project

---
+++ add.java
@@ -0,0 +1,10 @@
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=4, b=5, c;
+       c=a+b;
+       System.out.println("Sum of two numbers=" +c);
+	}
+
+}
